8d3a749335bfe895f733a7977fde0a4471f11fa3
PolyacovYury <kuf-1@yandex.ru>
2019-01-07 00:42:50 +0300

    Sanity check not passed

--- src/main/java/com/minelittlepony/ponynpcs/PonyNPCs.java
+++ src/main/java/com/minelittlepony/ponynpcs/PonyNPCs.java
@@ -8,7 +8,7 @@ import noppes.npcs.entity.EntityNpcPony;
 @Mod(modid = PonyNPCs.MODID, name = PonyNPCs.NAME, version = PonyNPCs.VERSION, clientSideOnly = true)
 @Mod.EventBusSubscriber
 public class PonyNPCs {
-    public static final String MODID = "PonyNPCs";
+    public static final String MODID = "ponynpcs";
     public static final String NAME = "PonyNPCs";
     public static final String VERSION = "1.0.0";
 
