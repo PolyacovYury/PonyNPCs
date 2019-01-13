ff40f4059f9f330bc28f1ace02e65ab2caf98da0
PolyacovYury <kuf-1@yandex.ru>
2019-01-13 23:24:06 +0300

    I am a client-side dedicated CNPCs addon. Also wrong event handler

--- src/main/java/com/minelittlepony/ponynpcs/PonyNPCs.java
+++ src/main/java/com/minelittlepony/ponynpcs/PonyNPCs.java
@@ -3,10 +3,15 @@ package com.minelittlepony.ponynpcs;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import noppes.npcs.entity.EntityNpcPony;
 
-@Mod(modid = PonyNPCs.MODID, name = PonyNPCs.NAME, version = PonyNPCs.VERSION, clientSideOnly = true)
+@Mod(modid = PonyNPCs.MODID,
+        name = PonyNPCs.NAME,
+        version = PonyNPCs.VERSION,
+        clientSideOnly = true,
+        dependencies = "required-client-after:customnpcs"  // I am a client-side dedicated CNPCs addon
+)
 @Mod.EventBusSubscriber
 public class PonyNPCs {
     public static final String MODID = "ponynpcs";
@@ -14,7 +19,7 @@ public class PonyNPCs {
     public static final String VERSION = "1.0.0";
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {  // Noppes uses a deprecated method, so I have to
+    public void Init(FMLInitializationEvent event) {  // Noppes uses a deprecated method, so I have to
         //noinspection unchecked,deprecation
         RenderingRegistry.registerEntityRenderingHandler(EntityNpcPony.class, new RenderPonyNpc(Minecraft.getMinecraft().getRenderManager()));
     }
