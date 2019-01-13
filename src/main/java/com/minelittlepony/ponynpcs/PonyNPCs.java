c44c602f12274141615a1fb1d5af1db40d43992d
PolyacovYury <kuf-1@yandex.ru>
2019-01-13 23:09:11 +0300

    My renderer is simply overwritten by Noppes' deprecated method usage. =_=

--- src/main/java/com/minelittlepony/ponynpcs/PonyNPCs.java
+++ src/main/java/com/minelittlepony/ponynpcs/PonyNPCs.java
@@ -1,5 +1,6 @@
 package com.minelittlepony.ponynpcs;
 
+import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -13,7 +14,8 @@ public class PonyNPCs {
     public static final String VERSION = "1.0.0";
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        RenderingRegistry.registerEntityRenderingHandler(EntityNpcPony.class, RenderPonyNpc::new);
+    public void preInit(FMLPreInitializationEvent event) {  // Noppes uses a deprecated method, so I have to
+        //noinspection unchecked,deprecation
+        RenderingRegistry.registerEntityRenderingHandler(EntityNpcPony.class, new RenderPonyNpc(Minecraft.getMinecraft().getRenderManager()));
     }
 }
